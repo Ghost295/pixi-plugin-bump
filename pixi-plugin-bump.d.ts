75996eb5dfdb71fd095f882d54bfafd704753a33
lycwed dev <ylechevalier@gmail.com>
2018-10-19 00:19:43 +0200

    fix d.ts

--- pixi-plugin-bump.d.ts
+++ pixi-plugin-bump.d.ts
@@ -5,31 +5,21 @@ declare namespace PIXI.extras {
     constructor();
     addCollisionProperties(sprite: PIXI.Sprite | PIXI.extras.AnimatedSprite): void;
     hitTestPoint(point: { x: number; y: number }, sprite: PIXI.Sprite | PIXI.extras.AnimatedSprite): void;
-    hitTestCircle(c1: PIXI.Circle, c2: PIXI.Circle, global: boolean = false): void;
-    circleCollision(c1: PIXI.Circle, c2: PIXI.Circle, bounce: boolean = false, global: boolean = false): void;
-    movingCircleCollision(c1: PIXI.Circle, c2: PIXI.Circle, global: boolean = false): void;
-    multipleCircleCollision(circlesArr: PIXI.Circle[], global: boolean = false): void;
-    rectangleCollision(r1: PIXI.Rectangle, r2, bounce: boolean = false, global: boolean = true): void;
-    hitTestRectangle(r1: PIXI.Rectangle, r2, global: boolean = false): void;
-    hitTestCircleRectangle(c1: PIXI.Circle, r1: PIXI.Rectangle, global: boolean = false): void;
-    hitTestCirclePoint(c1: PIXI.Circle, point: { x: number; y: number }, global: boolean = false): void;
-    circleRectangleCollision(
-      c1: PIXI.Circle,
-      r1: PIXI.Rectangle,
-      bounce: boolean = false,
-      global: boolean = false,
-    ): void;
-    circlePointCollision(
-      c1: PIXI.Circle,
-      point: { x: number; y: number },
-      bounce: boolean = false,
-      global: boolean = false,
-    ): void;
+    hitTestCircle(c1: PIXI.Circle, c2: PIXI.Circle, global: boolean): void;
+    circleCollision(c1: PIXI.Circle, c2: PIXI.Circle, bounce: boolean, global: boolean): void;
+    movingCircleCollision(c1: PIXI.Circle, c2: PIXI.Circle, global: boolean): void;
+    multipleCircleCollision(circlesArr: PIXI.Circle[], global: boolean): void;
+    rectangleCollision(r1: PIXI.Rectangle, r2, bounce: boolean, global: boolean): void;
+    hitTestRectangle(r1: PIXI.Rectangle, r2, global: boolean): void;
+    hitTestCircleRectangle(c1: PIXI.Circle, r1: PIXI.Rectangle, global: boolean): void;
+    hitTestCirclePoint(c1: PIXI.Circle, point: { x: number; y: number }, global: boolean): void;
+    circleRectangleCollision(c1: PIXI.Circle, r1: PIXI.Rectangle, bounce: boolean, global: boolean): void;
+    circlePointCollision(c1: PIXI.Circle, point: { x: number; y: number }, bounce: boolean, global: boolean): void;
     bounceOffSurface(o, s): void;
     contain(
       sprite: PIXI.Sprite | PIXI.extras.AnimatedSprite,
       container: PIXI.Container,
-      bounce: boolean = false,
+      bounce: boolean,
       extra: Function,
     ): void;
     outsideBounds(sprite: PIXI.Sprite | PIXI.extras.AnimatedSprite, bounds, extra: Function): void;
@@ -37,9 +27,9 @@ declare namespace PIXI.extras {
     hit(
       sprite: PIXI.Sprite | PIXI.extras.AnimatedSprite,
       spritesArr: PIXI.Sprite[] | PIXI.extras.AnimatedSprite[],
-      react: boolean = false,
-      bounce: boolean = false,
-      global: boolean = false,
+      react: boolean,
+      bounce: boolean,
+      global: boolean,
       extra: Function,
     ): void;
   }
